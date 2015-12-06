6d2450fa8bf2aa769ce3ae9981f96f99d024dcdb
Justin Abrahms <justin@abrah.ms>
2015-12-06 12:05:16 -0500

    days 1 & 2 with tests and a test script. Wee!

---
+++ test.sh
@@ -0,0 +1,6 @@
+sbcl --load ./deps/lisp-unit.lisp \
+     --load ./deps/split-sequence.lisp \
+     --load day1.lisp \
+     --load day2.lisp \
+     --eval "(run-tests)" \
+     --non-interactive 2> /tmp/advent_test.err | grep -E "TOTAL:.*0 failed*" || cat /tmp/advent_test.err
